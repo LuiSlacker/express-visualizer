5e1de76d9f7e8ac9561d5e2ddbb9f2f6538dd1c9
Petulant Slacker <l.goohsen@gmail.com>
2017-06-19 11:55:02 +0200

    added endpoint to fetch data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const Router = require('./Router');
+const path = require('path');
 
 module.exports = (app) => {
-  app.get('/visualize', (req, res, next) => {
-    res.json(new Router(app._router));
+  app.get('/visualize', (req, res, next) => { // eslint-disable-line no-unused-vars
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+
+  app.get('/fetchData', (req, res, next) => { // eslint-disable-line no-unused-vars
+    res.json(new Router(app._router)); // eslint-disable-line no-underscore-dangle
   });
 };
