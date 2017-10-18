f4768917b1ca5527d1d4a19509f7e02f767a3de7
Petulant Slacker <l.goohsen@gmail.com>
2017-10-18 13:25:56 -0500

    added check for production mode and updated README accordingly

--- src/index.js
+++ src/index.js
@@ -4,26 +4,21 @@ const Router = require('./core/Router');
 const path = require('path');
 
 module.exports = (app) => {
-  // Allow CORS
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+  if (process.env.NODE_ENV !== 'production') {
+    app.get('/expressVisualizer/visualize', (req, res, next) => { // eslint-disable-line no-unused-vars
+      res.sendFile(path.join(__dirname, './public/index.html'));
+    });
 
-  app.get('/expressVisualizer/visualize', (req, res, next) => { // eslint-disable-line no-unused-vars
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
+    app.get('/expressVisualizer/fetchData', (req, res, next) => { // eslint-disable-line no-unused-vars
+      res.json(new Router(app._router)); // eslint-disable-line no-underscore-dangle
+    });
 
-  app.get('/expressVisualizer/fetchData', (req, res, next) => { // eslint-disable-line no-unused-vars
-    res.json(new Router(app._router)); // eslint-disable-line no-underscore-dangle
-  });
+    app.get('/expressVisualizer/js/bundle.js', (req, res, next) => { // eslint-disable-line no-unused-vars
+      res.sendFile(path.join(__dirname, './public/js/bundle.js'));
+    });
 
-  app.get('/expressVisualizer/js/bundle.js', (req, res, next) => { // eslint-disable-line no-unused-vars
-    res.sendFile(path.join(__dirname, './public/js/bundle.js'));
-  });
-
-  app.get('/expressVisualizer/js/bundle.js.map', (req, res, next) => { // eslint-disable-line no-unused-vars
-    res.sendFile(path.join(__dirname, './public/js/bundle.js.map'));
-  });
+    app.get('/expressVisualizer/js/bundle.js.map', (req, res, next) => { // eslint-disable-line no-unused-vars
+      res.sendFile(path.join(__dirname, './public/js/bundle.js.map'));
+    });
+  }
 };
